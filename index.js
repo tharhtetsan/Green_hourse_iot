a142d56c113fd1568f50a7fd7f18c9b8b87af990
Thar Htet San <tharhtet1234@gmail.com>
2017-07-18 23:34:16 +0630

    Update index.js

--- index.js
+++ index.js
@@ -33,61 +33,7 @@ restService.post('/Action', function(req, res) {
               source: 'webhook-echo-sample'
                });
 
-                 if(Action_Name=="Heating Bulb Off")
-                 {
-                  sql="Update action_tb set act_1_status=0 where Mid='ths';";
-                 }
-                 else if(Action_Name=="Heating Bulb On")
-                 {
-                  sql="Update action_tb set act_1_status=1 where Mid='ths';";
-                
-                 }
-                 else if(Action_Name=="Humidity Off")
-                 {
-                    sql="Update action_tb set act_2_status=0 where Mid='ths';";
-                 }
-                 else if(Action_Name=="Humidity On")
-                 {
-                    sql="Update action_tb set act_2_status=1 where Mid='ths';";
-                
-                 }
-                 else if(Action_Name=="Sheet Off")
-                 {
-                    sql="Update action_tb set act_3_status=0 where Mid='ths';";
-                }
-                 else if(Action_Name=="Sheet On")
-                 {
-                    sql="Update action_tb set act_3_status=1 where Mid='ths';";
-                
-                 }
-                 else if(Action_Name=="Watering Off")
-                 {
-                    sql="Update action_tb set act_4_status=0 where Mid='ths';";
-                 }
-                 else if(Action_Name=="Watering On")
-                 {
-                    sql="Update action_tb set act_4_status=1 where Mid='ths';";
-                 }
-
-
-
-            if(sql=="")
-            {
-
-               return res.json({
-              speech: speech,
-              displayText: speech,
-              source: 'webhook-echo-sample'
-               });
-           }
-
-    speech=sql+"";
-    return res.json({
-        speech: speech,
-        displayText: speech,
-        source: 'webhook-echo-sample'
-    });
-
+           
 
 });
 
