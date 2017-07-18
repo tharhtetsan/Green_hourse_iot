c0351df8e680e4153868b9ea47f5c1572a43e588
Thar Htet San <tharhtet1234@gmail.com>
2017-07-18 23:37:27 +0630

    Update index.js

--- index.js
+++ index.js
@@ -21,17 +21,65 @@ restService.use(bodyParser.json());
 
 restService.post('/Action', function(req, res) {
 
-    var Action_Name=req.body.result;
+   var Action_Name=req.body.result;
 
                      var sql = "";
-                      var speech=eq.body.result+"\t____";
-    
-    
-       return res.json({
+                      var speech=""
+
+                 if(Action_Name=="Act_Heating_Bulb_Off")
+                 {
+                  sql="Update action_tb set act_1_status=0 where Mid='ths';";
+                 }
+                 else if(Action_Name=="Act_Heating_Bulb_On")
+                 {
+                  sql="Update action_tb set act_1_status=1 where Mid='ths';";
+                
+                 }
+                 else if(Action_Name=="Act_Humidity_Off")
+                 {
+                    sql="Update action_tb set act_2_status=0 where Mid='ths';";
+                 }
+                 else if(Action_Name=="Act_Humidity_On")
+                 {
+                    sql="Update action_tb set act_2_status=1 where Mid='ths';";
+                
+                 }
+                 else if(Action_Name=="Act_Sheet_Off")
+                 {
+                    sql="Update action_tb set act_3_status=0 where Mid='ths';";
+                }
+                 else if(Action_Name=="Act_Sheet_On")
+                 {
+                    sql="Update action_tb set act_3_status=1 where Mid='ths';";
+                
+                 }
+                 else if(Action_Name=="Act_Watering_Off")
+                 {
+                    sql="Update action_tb set act_4_status=0 where Mid='ths';";
+                 }
+                 else if(Action_Name=="Act_Watering_On")
+                 {
+                    sql="Update action_tb set act_4_status=1 where Mid='ths';";
+                 }
+
+
+
+            if(sql=="")
+            {
+
+               return res.json({
               speech: speech,
               displayText: speech,
               source: 'webhook-echo-sample'
                });
+           }
+
+    speech=sql+"";
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
 
            
 
