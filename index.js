ba277ffa280719bda23f439f63de2c0595e9dc41
Thar Htet San <tharhtet1234@gmail.com>
2017-07-18 22:45:53 +0630

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ restService.use(bodyParser.json());
 
 restService.post('/Action', function(req, res) {
 
-	var speech=req.intentName+"\t_____";
+	var speech=req.action+"\t_____";
     
     return res.json({
         speech: speech,
